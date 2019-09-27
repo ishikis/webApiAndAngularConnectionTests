5bd287ca9077df99fec948b1a3dfaafbeb859076
ishikis <ishik.is@hotmail.com>
2019-09-27 15:26:14 +0300

    angular web api çagırma temel

--- ngApp/src/app/app.component.ts
+++ ngApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngApp';
+  title: string = 'ngApp';
+  str: string = "";
+
+  constructor(private http: HttpClient) {
+    this.str = "";
+    // this.getValues().subscribe(x => {
+    //   debugger;
+    //   this.title = x
+    // // });
+
+    this.getValues().subscribe(x => {
+      this.str = x.toString();
+    });
+
+  }
+
+  getValues() {
+    return this.http.get("https://localhost:5001/api/values");
+  }
 }
